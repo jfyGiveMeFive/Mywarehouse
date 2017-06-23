7d3a22e6768607380477568586481f699bc060a2
Chuz NetTech <chuz@8bearsmacmini1.local>
2017-06-23 12:28:00 +0800

    111

--- GPUImageVideo/AppDelegate.m
+++ GPUImageVideo/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "WaterFliterViewController.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    WaterFliterViewController *waterVC = [[WaterFliterViewController alloc] init];
+    ViewController *waterVC = [[ViewController alloc] init];
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:waterVC];
     
     self.window.rootViewController = nav;
